bdb8f361112a8e6b11faf34b52bf66b295e62926
Bin Nguyen <binnd26052002@gmail.com>
2024-10-01 10:49:40 +0700

    V2

--- src/controllers/homeControllers.js
+++ src/controllers/homeControllers.js
@@ -1,8 +1,17 @@
-let getHomePage = (req,res) => {
-    return res.render('homepage.ejs');
+import db from '../models/index'
+import CRUDService from '../services/CRUDService';
+let getHomePage = async (req, res) => {
+    try {
+        let data = await db.User.findAll()
+        return res.render('homepage.ejs', {
+            data: JSON.stringify(data)
+        });
+    } catch (e) {
+        console.log(e);
+    }
 }
 
-let getAboutPage = (req,res) => {
+let getAboutPage = (req, res) => {
     return res.render('test/about.ejs');
 }
 
@@ -11,7 +20,61 @@ let getAboutPage = (req,res) => {
 //     value: ''
 // }
 
+let getCRUD = (req, res) => {
+    return res.render('crud.ejs');
+}
+
+let postCRUD = async (req, res) => {
+    let message = await CRUDService.createNewUser(req.body);
+    console.log(message);
+    return res.send('post crud from sever');
+}
+
+let displayGetCRUD = async (req, res) => {
+    let data = await CRUDService.getAllUser();
+    return res.render('displayCRUD.ejs', {
+        dataTable: data
+    })
+}
+
+let getEditCRUD = async (req, res) => {
+    let userId = req.query.id
+    if (userId) {
+        let userData = await CRUDService.getUserInfoById(userId);
+
+
+        return res.render('editCRUD.ejs', {
+            user: userData
+        })
+    } else {
+        return res.send('Users not Found!')
+    }
+}
+
+let putCRUD = async (req, res) => {
+    let data = req.body;
+    let allUsers = await CRUDService.updateUserData(data);
+    return res.render('displayCRUD.ejs', {
+        dataTable: allUsers
+    })
+}
+
+let deleteCRUD = async (req, res) => {
+    let id = req.query.id;
+    if (id) {
+        await CRUDService.deleteUserById(id);
+        return res.send('Delete the user success!')
+    } else {
+        return res.send('User not found!')
+    }
+}
 module.exports = {
     getHomePage: getHomePage,
-    getAboutPage: getAboutPage
+    getAboutPage: getAboutPage,
+    getCRUD: getCRUD,
+    postCRUD: postCRUD,
+    displayGetCRUD: displayGetCRUD,
+    getEditCRUD: getEditCRUD,
+    putCRUD: putCRUD,
+    deleteCRUD: deleteCRUD,
 }
--- src/route/web.js
+++ src/route/web.js
@@ -1,13 +1,21 @@
 import express from "express";
 import homeControllers from "../controllers/homeControllers";
+import userController from "../controllers/userController"
 
 let router = express.Router();
 
 let initWebRoutes = (app) => {
     router.get("/", homeControllers.getHomePage);
     router.get("/about", homeControllers.getAboutPage);
-
+    router.get("/crud", homeControllers.getCRUD);
+    router.post("/post-crud", homeControllers.postCRUD);
+    router.get("/get-crud", homeControllers.displayGetCRUD);
+    router.get("/edit-crud", homeControllers.getEditCRUD);
+    router.post('/put-crud', homeControllers.putCRUD);
+    router.get('/delete-crud', homeControllers.deleteCRUD)
     //rest api
+
+    router.post('/api/login', userController.handleLogin)
 
     return app.use("/", router);
 
